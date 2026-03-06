7fe643765c72994d44b9cb1aa401517f1fefcff9
vitomondino <vitomondino45@gmail.com>
2026-03-06 19:34:47 -0300

    modificacion archivo db.js

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,19 @@
 module.exports = {
   db: {
-    // Si la variable falla, usará el string directo
-    host: process.env.DB_HOST || 'mysql.railway.internal',
+    host: process.env.DB_HOST || 'mysql-production-8860.up.railway.app',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'mnImCLKFaTTSvifYXwrO1DZgWizTkBcL',
     database: process.env.DB_NAME || 'sistema-pelu',
-    port: Number(process.env.DB_PORT) || 3306,
-    ssl: false 
+    port: Number(process.env.DB_PORT) || 59742,
+    
+    // Configuración de túnel público
+    connectTimeout: 60000,
+    acquireTimeout: 60000,
+    waitForConnections: true,
+    connectionLimit: 10,
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
   server: {
     port: process.env.PORT || 8080,
