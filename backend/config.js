14a0837cebb7ef14acb1a443985e61d49878ed88
vitomondino <vitomondino45@gmail.com>
2026-03-07 10:38:46 -0300

    modificacion archivo db.js

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,11 @@
 module.exports = {
   db: {
-    host: process.env.DB_HOST || 'mysql.railway.internal',
+    host: process.env.DB_HOST || 'mysql.railway.internal', // Asegúrate de que diga mysql, no sistema-pelu
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'mnImCLKFaTTSvifYXwrO1DZgWizTkBcL',
     database: process.env.DB_NAME || 'sistema-pelu',
     port: Number(process.env.DB_PORT) || 3306,
-    ssl: false, // Desactivado para red interna
-    connectTimeout: 20000 // Aumentamos el tiempo por si el DNS interno tarda
+    ssl: false
   },
   server: {
     port: process.env.PORT || 8080
